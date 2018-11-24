500c862b91fe1f2cb406841d6e419ba6aa443c63
zerocloude <43513364+zerocloude@users.noreply.github.com>
2018-11-24 21:31:36 +0900

    Added num_iteration project

---
+++ num_iteration/LabWork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iteration
+{
+    class LabWork
+    {
+        static void Main()
+        {
+            float[,] Arr = { { 23, -5, -3, -6, -6 }, 
+                             { -5, 32, 9, -6, -5 }, 
+                             { -1, 7, 28, 5, 5 },
+                             { -3, -9, -7, 37, 9},
+                             { 4, 2, -6, -8, 26} };
+
+            float[] Brr = { 3, 50, 132, 54, 18 };
+
+            /*
+            float[,] Arr = { { 34, 9, -5, 1, 9},
+                             { 0, 24, 5, -6, 8},
+                             { 9, 8, 28, 2, -1},
+                             {-8, -8, -2, 34, 9},
+                             {9, 6, -4, -4, 35} };
+
+            float[] Brr = { 48, 62, 138, 50, 42 };
+            */
+
+            //  Задаем значения для решения СЛАУ
+            LinearSys Lab = new LinearSys(Arr, Brr, 0.001f);
+
+            //  Метод простых итераций
+            Lab.CalculateResult();
+
+            //  Вывод
+            Lab.Print();
+        }
+    }
+}
---
+++ num_iteration/LinearSys.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iteration
+{
+    class LinearSys
+    {
+        // Матрицы
+        float[,] A;
+        float[] B;
+        float[] X;
+        float Epsilon;
+        
+        // Размерность
+        readonly int Size;
+        
+        /* -------------------------------------------------------------------- */
+
+        public LinearSys(float[,] A, float[] B, float EpsValue = 0) 
+        // Конструктор
+        { 
+            this.A = A;
+            this.B = B;
+
+            Size = B.GetLength(0);
+            Epsilon = EpsValue;
+
+            X = new float[Size];
+        }
+
+        private bool CanIteraion()
+        //  Возможность применения метода простых итераций.
+        {
+            int count = 0;
+            for (int i = 0; i < Size; i++)
+            {
+                float Sum = 0;
+                for (int j = 0; (j < Size) && (j != i); j++)
+                {
+                    Sum += Math.Abs(A[i, j]);
+                }
+
+                if ((Math.Abs(A[i, i]) > Sum) && Math.Abs(A[i, i]) > 0)
+                    count++;
+            }
+            return count != Size ? false : true;
+        }
+
+        public void CalculateResult()
+        //  Метод простых итераций
+        {
+            if (!CanIteraion())
+            {
+                Console.WriteLine($"Невозможно провести метод простых итераций!");
+                return;
+            }
+
+            float[] PreviousValues = new float[Size];
+            float[] CurrentValues = new float [Size];
+
+            while (true)
+            {
+                int EpsCount = 0;
+                B.CopyTo(CurrentValues, 0);
+
+                /*  Нахождение текущих значений для решения СЛАУ */
+                for (int i = 0; i < Size; i++)
+                {
+                    for (int j = 0; j < Size; j++)
+                    {
+                        if (j != i)
+                        {
+                            CurrentValues[i] -= A[i, j] * PreviousValues[j];
+                        }
+                            
+                    }
+                    CurrentValues[i] /= A[i, i];
+                }
+
+                /*  Проверка точности */           
+                for (int i = 0; i < Size; i++)
+                {
+                    if (Math.Abs(CurrentValues[i] - PreviousValues[i]) < Epsilon)
+                        EpsCount++;
+                }
+
+                /*  Если все разности прошли проверку, то  
+                 *  текущие значения становятся результатом
+                 *  для решения СЛАУ */
+                if (EpsCount == Size)
+                {
+                    CurrentValues.CopyTo(X, 0);
+                    break;
+                }
+
+                /*  Если выше проверка не прошла  
+                 *  текущие значения становятся предыдущими */
+                CurrentValues.CopyTo(PreviousValues, 0);
+            }
+        }
+
+        /* -------------------------------------------------------------------- */
+
+        public void Print()
+        {
+            Print(A, "A");
+            Print(B, "B");
+            Print(X, "X");
+        }
+
+        public static void Print(float[,] Array, string Message = "Matrix")
+        //  Вывод матрицы
+        {
+            Console.WriteLine(Message + ":");
+            for (int i = 0; i < Array.GetLength(0); i++)
+            {
+                for (int j = 0; j < Array.GetLength(1); j++)
+                {
+                    Console.Write("{0,10}", Array[i, j]);
+                }
+                if (i != Array.Length)
+                    Console.WriteLine();
+            }
+        }
+
+        public static void Print(float[] Array, string Message = "List")
+        //  Вывод вектора
+        {
+            Console.WriteLine(Message + ":");
+            foreach (float item in Array)
+            {
+                Console.Write("{0,10}", $"{item:F4}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
