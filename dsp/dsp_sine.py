e7420b7ec1cca9970ab0872f7b12422fb8b8a09e
Zerocker <zerocloude@gmail.com>
2020-02-25 14:05:07 +0900

    Lab work №1

---
+++ dsp/dsp_sine.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Sine wave parameters
+frequency = 2
+amplitude = 5
+sample_rate = 32
+T = 1 / frequency
+Ts = 1 / sample_rate
+
+# Sine function
+def sinf(t):
+    return amplitude * np.sin(2*np.pi*frequency*t)
+
+# Nyquist–Shannon sampling theorem
+def sin_nsst(t, sr, fs):
+    ts = np.arange(0, 1+fs, fs)
+    sm = 0
+    for k in range(-len(ts), len(ts)):
+        sm += sinf(k/sr) * np.sinc(k - sr*t)
+
+    return ts, sm
+
+# Analog sine wave
+x = np.linspace(0, 1, sample_rate)
+y = sinf(x)
+
+# Recontructed sine wave
+xs, sm =  sin_nsst(x, sample_rate, Ts)
+ys = sinf(xs)
+
+# ---------------------------------------------------------------------
+
+# Drawing routine
+fig, axs = plt.subplots(2, sharex=True, sharey=True)
+fig.subplots_adjust(wspace=0.9, hspace=0.2)
+titles = ['Sine wave (Original)', 'Sine wave (NSST)']
+
+for i in range(2):
+    axs[i].grid(True)
+    axs[i].axhline(y=0, color='k')
+
+    axs[i].set_title(titles[i])
+    axs[i].set_xlabel('Time')
+    axs[i].set_ylabel('Amplitude')
+
+    if (i == 0):
+        axs[i].stem(x, y, markerfmt="", use_line_collection=True)
+        axs[i].plot(x, sm, 'o')
+    else:
+        axs[i].stem(xs, ys, markerfmt="", use_line_collection=True)
+
+plt.tight_layout()
+plt.show()
