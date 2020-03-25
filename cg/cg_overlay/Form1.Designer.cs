17ba803b4cb976748705097e7b833457761cada3
Zerocker <zerocloude@gmail.com>
2020-03-25 19:29:58 +0900

    CG: Overlay

---
+++ cg/cg_overlay/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.sfdFile = new System.Windows.Forms.SaveFileDialog();
+            this.mnuFileExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.ofdFile = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileOverlay = new System.Windows.Forms.ToolStripMenuItem();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.opacityNumUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sndRadioBtn = new System.Windows.Forms.RadioButton();
+            this.fstRadioBtn = new System.Windows.Forms.RadioButton();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.opacityNumUpDown)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(178, 6);
+            // 
+            // sfdFile
+            // 
+            this.sfdFile.Filter = "Image Files|*.bmp;*.png;*.jpg;*.gif;*.tif|All Files|*.*";
+            // 
+            // mnuFileExit
+            // 
+            this.mnuFileExit.Name = "mnuFileExit";
+            this.mnuFileExit.Size = new System.Drawing.Size(181, 22);
+            this.mnuFileExit.Text = "E&xit";
+            this.mnuFileExit.Click += new System.EventHandler(this.mnuFileExit_Click);
+            // 
+            // ofdFile
+            // 
+            this.ofdFile.FileName = "openFileDialog1";
+            this.ofdFile.Filter = "Image Files|*.bmp;*.png;*.jpg;*.gif;*.tif|All Files|*.*";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(458, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileOpen,
+            this.mnuFileOverlay,
+            this.toolStripSeparator1,
+            this.mnuFileExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // mnuFileOpen
+            // 
+            this.mnuFileOpen.Name = "mnuFileOpen";
+            this.mnuFileOpen.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.mnuFileOpen.Size = new System.Drawing.Size(181, 22);
+            this.mnuFileOpen.Text = "&Background";
+            this.mnuFileOpen.Click += new System.EventHandler(this.mnuFileOpen_Click);
+            // 
+            // mnuFileOverlay
+            // 
+            this.mnuFileOverlay.Name = "mnuFileOverlay";
+            this.mnuFileOverlay.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.mnuFileOverlay.Size = new System.Drawing.Size(181, 22);
+            this.mnuFileOverlay.Text = "O&verlay";
+            this.mnuFileOverlay.Click += new System.EventHandler(this.mnuFileOverlay_Click);
+            // 
+            // picImage
+            // 
+            this.picImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picImage.Location = new System.Drawing.Point(12, 97);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(100, 50);
+            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.picImage.TabIndex = 3;
+            this.picImage.TabStop = false;
+            this.picImage.Visible = false;
+            this.picImage.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picImage_MouseClick);
+            this.picImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.picImage_MouseMove);
+            // 
+            // opacityNumUpDown
+            // 
+            this.opacityNumUpDown.Location = new System.Drawing.Point(373, 25);
+            this.opacityNumUpDown.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.opacityNumUpDown.Name = "opacityNumUpDown";
+            this.opacityNumUpDown.Size = new System.Drawing.Size(52, 20);
+            this.opacityNumUpDown.TabIndex = 4;
+            this.opacityNumUpDown.Value = new decimal(new int[] {
+            127,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(314, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Opacity";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox1.Controls.Add(this.sndRadioBtn);
+            this.groupBox1.Controls.Add(this.fstRadioBtn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.opacityNumUpDown);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(431, 64);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control";
+            // 
+            // sndRadioBtn
+            // 
+            this.sndRadioBtn.AutoSize = true;
+            this.sndRadioBtn.Location = new System.Drawing.Point(111, 27);
+            this.sndRadioBtn.Name = "sndRadioBtn";
+            this.sndRadioBtn.Size = new System.Drawing.Size(81, 17);
+            this.sndRadioBtn.TabIndex = 7;
+            this.sndRadioBtn.Text = "Mask Mode";
+            this.sndRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // fstRadioBtn
+            // 
+            this.fstRadioBtn.AutoSize = true;
+            this.fstRadioBtn.Checked = true;
+            this.fstRadioBtn.Location = new System.Drawing.Point(14, 27);
+            this.fstRadioBtn.Name = "fstRadioBtn";
+            this.fstRadioBtn.Size = new System.Drawing.Size(91, 17);
+            this.fstRadioBtn.TabIndex = 6;
+            this.fstRadioBtn.TabStop = true;
+            this.fstRadioBtn.Text = "Overlay Mode";
+            this.fstRadioBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 165);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.picImage);
+            this.Name = "Form1";
+            this.Text = "Lab";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.opacityNumUpDown)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.SaveFileDialog sfdFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileExit;
+        private System.Windows.Forms.OpenFileDialog ofdFile;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileOverlay;
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.NumericUpDown opacityNumUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton fstRadioBtn;
+        private System.Windows.Forms.RadioButton sndRadioBtn;
+    }
+}
+
---
+++ cg/cg_overlay/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace Lab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mnuFileExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private Bitmap Background = null;
+
+        private Bitmap Foreground = null;
+
+        private Bitmap CombinedBitmap = null;
+
+        private Point FgLocation = new Point(0, 0);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+        }
+
+        /* Let the user open a file */
+        private void mnuFileOpen_Click(object sender, EventArgs e)
+        {
+            if (ofdFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Open the file.
+                    Background = new Bitmap(ofdFile.FileName);
+                    picImage.Image = Background;
+                    picImage.Visible = true;
+                    ClientSize = new Size(
+                        picImage.Right + picImage.Left,
+                        picImage.Bottom + picImage.Left);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error opening file.\n" + ex.Message,
+                        "Open Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        /* Load the overlay image */
+        private void mnuFileOverlay_Click(object sender, EventArgs e)
+        {
+            if (ofdFile.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Open the file.
+                    Foreground = new Bitmap(ofdFile.FileName);
+                    picImage.Cursor = Cursors.Cross;
+
+                    //Display the combined image.
+                    ShowCombinedImage();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error opening file.\n" + ex.Message,
+                        "Open Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+        /* Display the combined image */
+        private void ShowCombinedImage()
+        {
+            if (Background == null) return;
+
+            var opacity = Convert.ToByte(opacityNumUpDown.Value);
+            if (Foreground != null)
+            {
+
+                if (fstRadioBtn.Checked)
+                {
+                    CombinedBitmap = OverlayBitmap(Foreground, Background, opacity, FgLocation);
+                }
+                else if (sndRadioBtn.Checked)
+                {
+                    CombinedBitmap = MaskBitmap(Foreground, Background, opacity, FgLocation);
+                }
+            }
+            picImage.Image = CombinedBitmap;
+        }
+
+
+        /* Drag the overlay image */
+        private void picImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Foreground == null) return;
+
+            FgLocation = new Point(
+                e.X - Foreground.Width / 2,
+                e.Y - Foreground.Height / 2);
+            
+            ShowCombinedImage();
+        }
+
+
+        /* Finish dragging the overlay image */
+        private void picImage_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Foreground == null) return;
+            Foreground = null;
+            picImage.Cursor = Cursors.Default;
+
+            // Make the overlay permanent.
+            Background = CombinedBitmap;
+        }
+
+
+        /* Default overlay routine */
+        public Bitmap OverlayBitmap(Bitmap fg, Bitmap bg, byte opacity, Point location)
+        {
+            Bitmap output = new Bitmap(bg);
+
+            // Start at row indicated by location, or at row 0 if location.y is negative.
+            for (int y = Math.Max(location.Y, 0); y < bg.Height; y++)
+            {
+                // Due to translation
+                int fY = y - location.Y;    
+                
+                // Done with processing all rows of the foreground image
+                if (fY >= fg.Height) break;
+
+                // Start at col indicated by location, or at col 0 if location.y is negative.
+                for (int x = Math.Max(location.X, 0); x < bg.Width; x++)
+                {
+                    // Due to translation
+                    int fX = x - location.X;
+
+                    // Done with processing all cols of the foreground image
+                    if (fX >= fg.Width) break;
+
+                    // Get each pixel from both images
+                    Color fgPx = fg.GetPixel(fX, fY);
+                    Color bgPx = bg.GetPixel(x, y);
+                    
+                    // Determining the opacity of both images
+                    int aFg = (fgPx.A * opacity / 255);
+                    int aBg = bgPx.A;
+
+                    // Combine the background and foreground pixel, using the opacity
+                    int r = (fgPx.R * aFg + bgPx.R * (255 - aFg)) / 255;
+                    int g = (fgPx.G * aFg + bgPx.G * (255 - aFg)) / 255;
+                    int b = (fgPx.B * aFg + bgPx.B * (255 - aFg)) / 255;
+
+                    // Set a new pixel in result image
+                    output.SetPixel(x, y, Color.FromArgb(aBg, r, g, b));
+                }
+            }
+
+            // Well, f*ck it...
+            return output;
+        }
+
+        /* Mask overlay routine */
+        public Bitmap MaskBitmap(Bitmap fg, Bitmap bg, byte opacity, Point location)
+        {
+            Bitmap output = new Bitmap(bg);
+
+            for (int y = Math.Max(location.Y, 0); y < bg.Height; y++)
+            {
+                int fY = y - location.Y;
+
+                if (fY >= fg.Height) break;
+
+                for (int x = Math.Max(location.X, 0); x < bg.Width; x++)
+                {
+                    int fX = x - location.X;
+
+                    if (fX >= fg.Width) break;
+
+                    Color blendPx = fg.GetPixel(fX, fY);
+                    Color bgPx = bg.GetPixel(x, y);
+
+                    if (blendPx.A == 0)
+                    {
+                        output.SetPixel(x, y, bgPx);
+                    }
+                    else if (blendPx != bgPx)
+                    {
+                        float opF = blendPx.A / 255f;
+
+                        int r = Math.Max(0, Math.Min(255, (int)((bgPx.R - (opF) * blendPx.R) / (1 - opF))));
+                        int g = Math.Max(0, Math.Min(255, (int)((bgPx.G - (opF) * blendPx.G) / (1 - opF))));
+                        int b = Math.Max(0, Math.Min(255, (int)((bgPx.B - (opF) * blendPx.B) / (1 - opF))));
+
+                        output.SetPixel(x, y, Color.FromArgb(opacity, r, g, b));
+                    }
+                }
+            }
+
+            return output;
+        }
+    }
+}
